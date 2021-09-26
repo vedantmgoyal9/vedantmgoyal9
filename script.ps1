45989b2bda496442686cc46a768d3adcd16c85af
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-09-26 10:19:35 +0000

    replace name with pkgid

--- script.ps1
+++ script.ps1
@@ -14,7 +14,7 @@ foreach ($package in $packages) {
     $urls.Clear()
     $result = Invoke-RestMethod -Headers $header -Uri "https://api.github.com/repos/$($package.repo)/releases/latest" -UseBasicParsing | Select-Object name,tag_name,assets,prerelease
     if ($result.prerelease -eq $package.is_prerelease -and $result.tag_name -gt $package.last_checked_tag) {
-        Write-Host -ForegroundColor Green "Found update for`: $($package.name)"
+        Write-Host -ForegroundColor Green "Found update for`: $($package.pkgid)"
         # Get download urls using regex pattern and add to array
         foreach ($asset in $result.assets) {
             if ($asset.name -match $package.asset_regex) {
@@ -41,7 +41,7 @@ foreach ($package in $packages) {
     }
     else
     {
-        Write-Host -ForegroundColor 'DarkYellow' "No updates found for:` $($package.name)"
+        Write-Host -ForegroundColor 'DarkYellow' "No updates found for:` $($package.pkgid)"
     }
 }
 # Update packages.json in repository
