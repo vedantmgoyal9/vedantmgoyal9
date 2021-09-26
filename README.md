c0a743dd03362a7852c16b2e816c280dd39b3a28
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-09-26 12:54:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# winget-pkgs-automation
-This project is for auto-updating manifests of packages that are shipped through GitHub releases.
+# WinGet Community Repository Manifests Automatic Update
+This project is to auto-update manifests of packages which use github releases.
+> At this time, packages which are not shipped through GitHub Releases are not supported for automatic updation. If you have some suggestions about automatic updation of manifests for these packages, please open a new issue/discussion and discuss them there.
+
+# How to add a package to the automation?
+Head over to issues tab, and click New Issue. Select **New Package** and fill in the information and wait for the package to get added to automation.
+
+You can also directly open a PR to add the package to packages.json.
+
+# Contributions
+This project welcomes contributions and suggestions.
+
+## Issues
+Feel free to submit issues, bugs, and enhancement requests.
+
+## Pull Requests
+You can open PRs to add packages to packages.json file, so that it is automatically updated at [Windows Package Manager Community Repository](https://github.com/microsoft/winget-pkgs).
+
+If you are opening a PR to make changes in the script, be sure to describe the changes in the PR Comment.
---
+++ script.ps1
@@ -0,0 +1,37 @@
+Invoke-WebRequest 'https://aka.ms/wingetcreate/latest/self-contained' -OutFile wingetcreate.exe # Get wingetcreate-self-contained
+wingetcreate.exe token --store $super_secret_information # Store the token
+$header = @{
+    Authorization = 'Basic {0}' -f $([System.Convert]::ToBase64String([char[]]"vedantmgoyal2009:$super_secret_information"))
+    Accept = 'application/vnd.github.v3+json'
+}
+$packages = Get-Content -Path "./packages.json" -Raw | ConvertFrom-Json
+$urls = [System.Collections.ArrayList]::new()
+ForEach ($package in $packages) {
+    $urls.Clear()
+    $result = Invoke-RestMethod -Headers $header -Uri "https://api.github.com/repos/$($package.repo)/releases/latest" -usebasicparsing | Select-Object name,tag_name,assets,prerelease
+    if ($result.prerelease -eq $package.is_prerelease -and $result.tag_name -gt $package.last_checked_tag) {
+        # Get download urls using regex pattern and add to array
+        foreach ($asset in $result.assets) {
+            if ($asset.name -match $package.asset_regex) {
+                $urls.Add($asset.browser_download_url) | Out-Null
+            }
+        }
+        # Get the latest version of the package using method specified in the packages.json till microsoft/winget-create#177 is resolved
+        switch ($package.version_method) {
+            "jackett" { $version = "$($result.tag_name.TrimStart("v")).0"; break }
+            "clink" { $version = ($urls[0] | Select-String -Pattern "[0-9]\.[0-9]\.[0-9]{1,2}\.[A-Fa-f0-9]{6}").Matches.Value; break }
+            default { $version = $result.tag_name.TrimStart("v"); break }
+        }
+        # Generate manifests and submit to winget community repository
+        wingetcreate.exe update $package.pkgid --urls $($urls.ToArray() -join " ") --version $version
+        # Update the last_checked_tag in the packages.json
+        $file = $packages 
+        $file[$packages.IndexOf($package)].last_checked_tag = $result.tag_name
+        $file | ConvertTo-Json > packages.json
+    }
+    else
+    {
+        Write-Host -ForegroundColor 'DarkYellow' "No updates found for:` $($package.name)"
+    }
+}
+wingetcreate.exe token --clear
