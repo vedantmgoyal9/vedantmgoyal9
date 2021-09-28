f9529264b1204f26db354a3b492046539cbd6e4a
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-09-28 17:00:28 +0000

    migrating from wingetcreate to yamlcreate

--- initial_setup.ps1
+++ initial_setup.ps1
@@ -1,13 +1,26 @@
 # Hide progress bar of Invoke-WebRequest
 $ProgressPreference = 'SilentlyContinue';
-# Get wingetcreate-self-contained
-Write-Host -ForegroundColor Green "Downloading wingetcreate-self-contained"
-Invoke-WebRequest 'https://aka.ms/wingetcreate/latest/self-contained' -OutFile wingetcreate.exe
-# Store the token
-.\wingetcreate.exe token --store --token $env:super_secret_information | Out-Null
-Write-Host -ForegroundColor Green "Token stored successfully."
+# GitHub CLI Authentication
+gh auth login --with-token $env:GITHUB_TOKEN
+# Clone forked repository
+gh repo clone winget-pkgs
+# Jump into Tools directory
+$currentDir = Get-Location # Get current directory
+Set-Location .\winget-pkgs\Tools
+# Get YamlCreate Unattended Script
+Write-Host -ForegroundColor Green "Downloading YamlCreate.ps1 Unattended"
+Invoke-WebRequest 'https://aka.ms/wingetcreate/latest/self-contained' -OutFile YamlCreate.ps1
+# Stash changes
+Write-Host -ForegroundColor Green "Stashing changes [YamlCreate.ps1]"
+git stash
+# Go to previous working directory
+Set-Location $currentDir
+# YamlCreate Function
+Function YamlCreate ([string]$PackageIdentifier, [string]$PackageVersion, $Urls) {
+    .\winget-pkgs\Tools\YamlCreate.ps1 -PackageIdentifier $PackageIdentifier -PackageVersion $PackageVersion -Mode 2 -Urls $Urls
+}
 # Set up API headers
 $header = @{
-    Authorization = 'Basic {0}' -f $([System.Convert]::ToBase64String([char[]]"vedantmgoyal2009:$env:super_secret_information"))
+    Authorization = 'Basic {0}' -f $([System.Convert]::ToBase64String([char[]]"vedantmgoyal2009:$env:GITHUB_TOKEN"))
     Accept = 'application/vnd.github.v3+json'
 }
