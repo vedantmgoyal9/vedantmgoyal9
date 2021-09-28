1eddbdbd1f91c4a6e7d3a29d4db71a55bcd3bbe9
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-09-28 02:47:33 +0000

    main.ps1 for single-packages

---
+++ single-packages/main.ps1
@@ -0,0 +1,13 @@
+# Download wingetcreate.exe, store token information and setup API headers
+..\initial_setup.ps1
+.\signal.ps1
+# Push script with updated last_checked_tag to repository
+Write-Host -ForegroundColor Green "`nUpdating single-packages.json"
+git config --global user.name 'winget-pkgs-automation'
+git config --global user.email '83997633+vedantmgoyal2009@users.noreply.github.com'
+git pull # to be on a safe side
+git add .\single-packages.json
+git commit -m "Update single-packages.json [$env:GITHUB_RUN_NUMBER]"
+git push
+# Clear authentication information
+.\wingetcreate.exe token --clear
--- single-packages/signal.ps1
+++ single-packages/signal.ps1
@@ -1,5 +1,3 @@
-# Download wingetcreate.exe, store token information and setup API headers
-..\initial_setup.ps1
 # Get last_checked_tag of signal from single-packages.json
 $pkginfo = Get-Content single-packages.json | ConvertFrom-Json
 $result = Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/signalapp/Signal-Desktop/releases" -UseBasicParsing -Method Get | ConvertFrom-Json | Select-Object -Property tag_name,prerelease -First 1
@@ -15,7 +13,7 @@ if ($result.prerelease -eq $true -and $pkginfo.signal.last_checked_tag_beta -gt 
     $pkginfo.signal.last_checked_tag = $result.tag_name
 } else {
     Write-Host -ForegroundColor Green "No updates found."
-    exit 0
+    return
 }
 # Print the update information
 Write-Host -ForegroundColor Green "Update found for`: $pkgid"
@@ -25,14 +23,6 @@ Write-Host -ForegroundColor Green "InstallerUrl`: $url"
 Write-Host -ForegroundColor Green "Submitting manifests to repository"
 .\wingetcreate.exe update $pkgid --version $version --submit --urls $url
 # Update the last_checked_tag variable
+Write-Host -ForegroundColor Green "Updating last_checked_tag for signal"
 $pkginfo > single-packages.json
-# Push script with updated last_checked_tag to repository
-Write-Host -ForegroundColor Green "`nUpdating single-packages.json"
-git config --global user.name 'winget-pkgs-automation'
-git config --global user.email '83997633+vedantmgoyal2009@users.noreply.github.com'
-git pull # to be on a safe side
-git add .\single-packages.json
-git commit -m "Update single-packages.json [$env:GITHUB_RUN_NUMBER]"
-git push
-# Clear authentication information
-.\wingetcreate.exe token --clear
+Write-Host -ForegroundColor Green "Done`n"
