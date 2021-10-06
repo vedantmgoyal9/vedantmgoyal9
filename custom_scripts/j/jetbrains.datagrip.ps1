f14e22d8f329e2d5b9de65edf901ca690f8ee1a6
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-06 18:33:46 +0000

    add files part 1

---
+++ custom_scripts/j/jetbrains.datagrip.ps1
@@ -0,0 +1,9 @@
+$result = Invoke-WebRequest -Headers $header -Uri "https://data.services.jetbrains.com/products/releases?latest=true&type=release&code=DG" -UseBasicParsing -Method Get | ConvertFrom-Json | Select-Object -ExpandProperty DG
+if ($result.build -gt $package.last_checked_tag) {
+    $version = $result.build
+    $urls.Add($result.downloads.windows.link -replace "https://download.jetbrains.com","https://download-cdn.jetbrains.com")
+}
+else
+{
+    Write-Host -ForegroundColor 'DarkYellow' "No updates found for`: $($package.pkgid)"
+}
