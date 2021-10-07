64cbf80c14406abb6fdb5877670a8ed1cd78ef79
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-07 17:45:33 +0000

    add google chrome

---
+++ custom_scripts/g/google.chrome.ps1
@@ -0,0 +1,13 @@
+$result = (Invoke-WebRequest -Uri $package.repo -UseBasicParsing | ConvertFrom-Json).versions | Where-Object { $_.os -eq 'win' -and $_.channel -eq 'stable'}
+if ($result.current_version -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.current_version
+    $jsonTag = $result.current_version
+    $urls.Add("https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi") | Out-Null
+    $urls.Add("https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise.msi") | Out-Null
+}
+else
+{
+    $update_found = $false
+}
