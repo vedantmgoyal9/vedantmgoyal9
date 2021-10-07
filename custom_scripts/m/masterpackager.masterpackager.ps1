2d946c6b58a7a0b59eb1a9f281cbbd6dd9067174
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-07 18:04:05 +0000

    add masterpackager

---
+++ custom_scripts/m/masterpackager.masterpackager.ps1
@@ -0,0 +1,12 @@
+$result = (Invoke-WebRequest -Uri $package.repo -UseBasicParsing).Content
+if ($result -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = "$result.0"
+    $jsonTag = $result
+    $urls.Add("https://www.masterpackager.com/installer/public/standard/masterpackager_$result.0.msi") | Out-Null
+}
+else
+{
+    $update_found = $false
+}
