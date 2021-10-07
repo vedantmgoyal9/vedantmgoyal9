16b57c305f6c416c06675a77cd80fc9279bc82cc
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-07 13:20:51 +0530

    Add all packages of JetBrains (#27)
    * add files part 1
    
    * Hub
    
    * IDEA Community
    
    * IDEA Edu
    
    * Add progress markers
    
    * Change awwbees.BespokeSynth repo (#29)
    
    * Skip awwbees.BespokeSynth
    
    * Update awwbees.bespokesynth.json
    
    Co-authored-by: Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
    
    * Move JSONs
    
    * codespace settings
    
    * phpstorm
    
    * Add scripts
    
    * JSONs for all packages part 1
    
    * JSONs for all packages part 2
    
    * Move to packages/j directory
    
    * Delete settings.json [vscode]
    
    Co-authored-by: Kaleb Luedtke <trenlymc@gmail.com>

---
+++ custom_scripts/j/jetbrains.mps.eap.ps1
@@ -0,0 +1,9 @@
+$result = Invoke-WebRequest -Headers $header -Uri "https://data.services.jetbrains.com/products/releases?latest=true&type=eap&code=MPS" -UseBasicParsing -Method Get | ConvertFrom-Json | Select-Object -ExpandProperty MPS
+if ($result.build -gt $package.last_checked_tag) {
+    $version = "MPS-$result.build"
+    $urls.Add($result.downloads.windows.link -replace "https://download.jetbrains.com","https://download-cdn.jetbrains.com")
+}
+else
+{
+    Write-Host -ForegroundColor 'DarkYellow' "No updates found for`: $($package.pkgid)"
+}
