3ee30e7fa45f513116bbb7b5581a1d0f382adfda
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-08 03:39:48 +0000

    Add Oracle.VirtualBox

---
+++ custom_scripts/o/oracle.virtualbox.ps1
@@ -0,0 +1,18 @@
+$result = (Invoke-WebRequest -Uri $package.repo -UseBasicParsing).Content
+if ($result -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result
+    $jsonTag = $result
+    (Invoke-WebRequest -Uri "https://download.virtualbox.org/virtualbox/6.1.26/").Links.outerHTML | ForEach-Object {
+        if ($_ -match ".*(exe)") {
+            $fileName = ($_ -replace "<a.*`">","" -replace "</a>","")
+        }
+    }
+    $installerUrl = "https://download.virtualbox.org/virtualbox/$result/$fileName"
+    $urls.Add($installerUrl) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
