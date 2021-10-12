39613ef2831718b8ee59011e6435dd9868e09e65
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-12 09:42:19 +0000

    version_method: RandyRants.SharpKeys

--- automation.ps1
+++ automation.ps1
@@ -101,6 +101,7 @@ foreach ($json in $packages) {
                     "picard" { $version = "$($result.tag_name.TrimStart("release-"))0000.0"; break }
                     "dosbox" { $version = "$($result.tag_name.TrimStart("dosbox-x-v"))"; break }
                     "authpass" { $version = ($urls[0] | Select-String -Pattern "[0-9]\.[0-9]\.[0-9]_[0-9]{4}").Matches.Value; break }
+                    "randyrants.sharpkeys" { $version = "$($result.tag_name)000)"; break }
                     default { $version = $result.tag_name.TrimStart("v"); break }
                 }                
                 # Print update information, generate and submit manifests, updates the last_checked_tag in json
