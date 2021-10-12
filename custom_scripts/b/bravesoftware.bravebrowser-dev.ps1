c9ca8af97d7a8d6cc5ead611b6a1dca46bef0026
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-12 10:19:27 +0000

    enable bravesoftware.bravebrowser-dev

---
+++ custom_scripts/b/bravesoftware.bravebrowser-dev.ps1
@@ -0,0 +1,18 @@
+$result = $(Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/$($package.repo)/releases?per_page=1" -UseBasicParsing -Method Get | ConvertFrom-Json)[0] | Select-Object -Property name,tag_name,assets -First 1
+if ($result.tag_name -gt $package.last_checked_tag -and $result.name.Contains("Dev"))
+{
+    $update_found = $true
+    $version = "94.$($result.tag_name)"
+    $jsonTag = $result.tag_name
+    foreach ($asset in $result.assets)
+    {
+        if ($asset.name -match $package.asset_regex)
+        {
+            $urls.Add($asset.browser_download_url) | Out-Null
+        }
+    }
+}
+else
+{
+    $update_found = $false
+}
