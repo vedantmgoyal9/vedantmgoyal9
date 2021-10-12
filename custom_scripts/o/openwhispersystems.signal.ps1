a206f957d2e933cc4cafada0971d67b3fe60af62
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-12 18:10:41 +0000

    Add signal desktop (stable+beta)

---
+++ custom_scripts/o/openwhispersystems.signal.ps1
@@ -0,0 +1,12 @@
+$result = $(Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/$($package.repo)/releases?per_page=1" -UseBasicParsing -Method Get | ConvertFrom-Json)[0] | Select-Object -Property tag_name,assets,prerelease -First 1
+if ($result.prerelease -eq $package.is_prerelease -and $result.tag_name -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.tag_name.TrimStart('v')
+    $jsonTag = $result.tag_name
+    $urls.Add("https://updates.signal.org/desktop/signal-desktop-win-$version.exe") | Out-Null
+}
+else
+{
+    $update_found = $false
+}
