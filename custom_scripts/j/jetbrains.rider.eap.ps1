039853deb4943c4c63ebdacc8b16cb665dd96f99
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-12 05:35:53 +0000

    add jetbrains rider (release+eap)

---
+++ custom_scripts/j/jetbrains.rider.eap.ps1
@@ -0,0 +1,12 @@
+$result = Invoke-WebRequest -Headers $header -Uri "https://data.services.jetbrains.com/products/releases?latest=true&type=eap&code=RD" -UseBasicParsing -Method Get | ConvertFrom-Json | Select-Object -ExpandProperty RD
+if ($result.build -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.build
+    $jsonTag = $result.build
+    $urls.Add(($result.downloads.windows.link -replace "https://download.jetbrains.com","https://download-cdn.jetbrains.com")) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
