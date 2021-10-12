b2151d75bcdde129f653576fd46549744351ffe6
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-12 18:58:51 +0530

    Microsoft Edge (all channels) (#37)
    * Add Microsoft Edge
    
    * Update packages.txt
    
    * Update microsoft.edgebeta.json
    
    * Update microsoft.edgedev.json

---
+++ custom_scripts/m/microsoft.edge.ps1
@@ -0,0 +1,14 @@
+$result = Invoke-WebRequest -Uri $package.repo -UseBasicParsing | ConvertFrom-Json | Where-Object { $_.Product -eq 'Stable' } | Select-Object -ExpandProperty Releases | Sort-Object -Property ProductVersion -Descending | Where-Object { $_.Platform -like "Windows" }
+if ($result[0].ProductVersion -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result[0].ProductVersion
+    $jsonTag = $result[0].ProductVersion
+    $urls.Add($result[0].Artifacts.Location)
+    $urls.Add($result[1].Artifacts.Location)
+    $urls.Add($result[2].Artifacts.Location)
+}
+else
+{
+    $update_found = $false
+}
