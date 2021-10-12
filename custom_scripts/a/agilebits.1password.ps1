51332c7e68305da896db2de32d6b6f5f7687c9bd
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-12 10:56:24 +0000

    add AgileBits.1Password

---
+++ custom_scripts/a/agilebits.1password.ps1
@@ -0,0 +1,12 @@
+$result = (Invoke-WebRequest -Uri $package.repo -UseBasicParsing | ConvertFrom-Json).rules | Select-Object -Last 1
+if ($result.before -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.before
+    $jsonTag = $result.before
+    $urls.Add($result.url) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
