5b72d40e2e5cba291b584c60273de3bda2cbcfe4
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-14 17:13:53 +0000

    Resolves #65 (Glodon.CADReaderInternational)

---
+++ custom_scripts/g/glodon.cadreaderinternational.ps1
@@ -0,0 +1,12 @@
+$result = (Invoke-WebRequest -Uri $package.repo -UseBasicParsing | ConvertFrom-Json).data
+if ($result.version -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.version
+    $jsonTag = $result.version
+    $urls.Add($result.url) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
