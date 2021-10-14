b5f7c4c05b30f85a4b0df53a66002091295db060
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-14 11:03:45 +0000

    add rproject.r

---
+++ custom_scripts/r/rproject.r.ps1
@@ -0,0 +1,12 @@
+$result = ((Invoke-WebRequest -Uri $package.repo -UseBasicParsing).RawContent | Select-String -Pattern "Release: [0-9.]{5,6}").Matches.Value.TrimStart("Release: ")
+if ($result -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result
+    $jsonTag = $result
+    $urls.Add("https://cran.microsoft.com/bin/windows/base/R-$version-win.exe") | Out-Null
+}
+else
+{
+    $update_found = $false
+}
