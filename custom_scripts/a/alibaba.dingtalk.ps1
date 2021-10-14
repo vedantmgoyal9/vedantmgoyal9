7ac267dfc80cfab8bf676081b23fbe5f2d340e57
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-14 15:18:51 +0000

    Add Alibaba.DingTalk

---
+++ custom_scripts/a/alibaba.dingtalk.ps1
@@ -0,0 +1,14 @@
+Invoke-WebRequest -Uri $package.repo -UseBasicParsing -OutFile ./release_windows_vista_later_manual_lowpriority.json
+$result = (Get-Content -Raw -Path ./release_windows_vista_later_manual_lowpriority.json | ConvertFrom-Json).win.install
+Remove-Item release_windows_vista_later_manual_lowpriority.json
+if ($result.version -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.version
+    $jsonTag = $result.version
+    $urls.Add($result.url) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
