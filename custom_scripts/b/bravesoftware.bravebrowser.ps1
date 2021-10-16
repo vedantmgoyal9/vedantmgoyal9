34874011783f2c7f3d40cfe966b12ba661c67bc2
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-17 01:44:28 +0530

    Check release using release id instead of tag_name (#91)

--- custom_scripts/b/bravesoftware.bravebrowser.ps1
+++ custom_scripts/b/bravesoftware.bravebrowser.ps1
@@ -1,9 +1,9 @@
-$result = $(Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/$($package.repo_uri)/releases?per_page=1" -UseBasicParsing -Method Get | ConvertFrom-Json)[0] | Select-Object -Property name,tag_name,assets -First 1
-if ($result.tag_name -gt $package.last_checked_tag -and $result.name.Contains("Release"))
+$result = $(Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/$($package.repo_uri)/releases?per_page=1" -UseBasicParsing -Method Get | ConvertFrom-Json)[0] | Select-Object -Property name,id,tag_name,assets -First 1
+if ($result.id -gt $package.last_checked_tag -and $result.name.Contains("Release"))
 {
     $update_found = $true
     $version = "95.$($result.tag_name.TrimStart("v"))"
-    $jsonTag = $result.tag_name
+    $jsonTag = $result.id.ToString()
     foreach ($asset in $result.assets)
     {
         if ($asset.name -match $package.asset_regex)
--- custom_scripts/o/openwhispersystems.signal.ps1
+++ custom_scripts/o/openwhispersystems.signal.ps1
@@ -1,9 +1,9 @@
-$result = $(Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/$($package.repo_uri)/releases?per_page=1" -UseBasicParsing -Method Get | ConvertFrom-Json)[0] | Select-Object -Property tag_name,assets,prerelease -First 1
-if ($result.prerelease -eq $package.is_prerelease -and $result.tag_name -gt $package.last_checked_tag)
+$result = $(Invoke-WebRequest -Headers $header -Uri "https://api.github.com/repos/$($package.repo_uri)/releases?per_page=1" -UseBasicParsing -Method Get | ConvertFrom-Json)[0] | Select-Object -Property id,tag_name,assets,prerelease -First 1
+if ($result.prerelease -eq $package.is_prerelease -and $result.id -gt $package.last_checked_tag)
 {
     $update_found = $true
     $version = $result.tag_name.TrimStart('v')
-    $jsonTag = $result.tag_name
+    $jsonTag = $result.id.ToString()
     $urls.Add("https://updates.signal.org/desktop/signal-desktop-win-$version.exe") | Out-Null
 }
 else
