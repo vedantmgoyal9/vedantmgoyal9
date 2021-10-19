ef346d91fd626929eac63b6c15bcf138d61ee2f7
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-19 04:19:54 +0000

    Print messages in VerifyArpData.ps1

--- scripts/VerifyArpData.ps1
+++ scripts/VerifyArpData.ps1
@@ -2,12 +2,21 @@ Param (
     [string]$ManifestPath
 )
 
+Write-Host -ForegroundColor Green "Installing and verifying ARP Metadata..."
+
 $manifestInfo = winget show --manifest $ManifestPath
 $pkgVersion = ($manifestInfo | Select-String "Version:").ToString().TrimStart("Version:").Trim()
 $pkgPublisher = ($manifestInfo | Select-String "Publisher:").ToString().TrimStart("Publisher:").Trim()
+
+Write-Host -ForegroundColor Green "PackageVersion in manifest: $pkgVersion"
+Write-Host -ForegroundColor Green "PackagePublisher in manifest: $pkgPublisher"
+
 winget install --manifest $ManifestPath
 Get-WmiObject -Class Win32_InstalledWin32Program | Select-Object Name, Vendor, Version | Out-Null
 $arpData = Get-WmiObject -Class Win32_InstalledWin32Program | Select-Object Name, Vendor, Version
+
+Write-Host -ForegroundColor Green "Checking ARP entries..."
+Write-Host $arpData
 
 if (-not $pkgPublisher -in $arpData.Vendor) {
     $PrePrBodyContent = @"
