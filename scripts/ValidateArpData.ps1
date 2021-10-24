ad3446b201dbba3edcd76fa1bd7ed0924cb63c41
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-24 07:23:24 +0000

    Use WMI class as registry isn't best approach here

--- scripts/ValidateArpData.ps1
+++ scripts/ValidateArpData.ps1
@@ -4,12 +4,15 @@ Param (
 
 $getArpEntriesFunctions = {
     Function Get-ARPTable {
-        $registry_paths = @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
-        return Get-ItemProperty $registry_paths -ErrorAction SilentlyContinue |
-        Select-Object DisplayName, DisplayVersion, Publisher, @{N = 'ProductCode'; E = { $_.PSChildName } } |
-        Where-Object { $null -ne $_.DisplayName } |
-        Where-Object { $null -ne $_.DisplayVersion } |
-        Where-Object { $null -ne $_.Publisher }
+        # $registry_paths = @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
+        # return Get-ItemProperty $registry_paths -ErrorAction SilentlyContinue |
+        # Select-Object DisplayName, DisplayVersion, Publisher, @{N = 'ProductCode'; E = { $_.PSChildName } } |
+        # Where-Object { $null -ne $_.DisplayName } |
+        # Where-Object { $null -ne $_.DisplayVersion } |
+        # Where-Object { $null -ne $_.Publisher }
+        # I know this is not the best way to do this, but I think it works better here than the registry approach
+        Get-WmiObject -Class Win32_InstalledWin32Program | Select-Object Name, Vendor, Version | Out-Null # refresh
+        return Get-WmiObject -Class Win32_InstalledWin32Program | Select-Object Name, Vendor, Version
     }
 
     # See how the ARP table changes before and after a ScriptBlock.
@@ -23,7 +26,7 @@ $getArpEntriesFunctions = {
         $currentArp = Get-ARPTable
         return (Compare-Object $currentArp $originalArp).InputObject
     }
-
+    # Usage when using registry approach:
     # Example usage:
     # Get-ARPTableDifference { winget install Microsoft.Teams; }
     # Returns:
@@ -54,10 +57,10 @@ if ($installJob.State -eq "Completed") {
 if ($installationSuccessful -eq $true) {
     Write-Host -ForegroundColor Green "Successfully installed the package."
     Write-Host -ForegroundColor Green "Checking ARP entries..."
-    if (-not $pkgPublisher -eq $difference.Publisher) {
+    if (-not $pkgPublisher -eq $difference.Vendor) {
         Write-Host -ForegroundColor Yellow "Publisher in the manifest is different from the one in the ARP."
         $PrePrBodyContent = "### Publisher in the manifest is different from the one in the ARP.`nPublisher in Manifest`: $pkgPublisher`nPublisher in ARP`: $($arpData.Vendor)"
-    } elseif (-not $pkgVersion -eq $difference.DisplayVersion) {
+    } elseif (-not $pkgVersion -eq $difference.Version) {
         Write-Host -ForegroundColor Yellow "Version in the manifest is different from the one in the ARP."
         $PrePrBodyContent = "### Package version in the manifest is different from the one in the ARP.`nVersion in Manifest: $pkgVersion`nVersion in ARP: $($arpData.Version)"
     } else {
