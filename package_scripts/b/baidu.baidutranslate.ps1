bc353b9b23004546bce8e676f9023620e57876db
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-10-27 13:03:52 +0000

    Add Baidu.BaiduTranslate (Resolves #113)

--- package_scripts/b/baidu.baidutranslate.ps1
+++ package_scripts/b/baidu.baidutranslate.ps1
@@ -1,10 +1,10 @@
-$result = Invoke-WebRequest -Uri $package.repo_uri -UseBasicParsing | ConvertFrom-Yaml
+$result = [System.Text.Encoding]::UTF8.GetString((Invoke-WebRequest -Uri $package.repo_uri -UseBasicParsing).RawContentStream.ToArray()) | ConvertFrom-Yaml
 if ($result.version -gt $package.last_checked_tag)
 {
     $update_found = $true
     $version = $result.version
     $jsonTag = $result.version
-    $urls.Add($result.path) | Out-Null
+    $urls.Add([Uri]::EscapeUriString($result.path)) | Out-Null
 }
 else
 {
