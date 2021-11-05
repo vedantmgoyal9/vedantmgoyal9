46c64d72eee3d4b8888693784a3137c8a87635ad
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-11-05 13:21:30 +0000

    LAIPIC.Perfoo (Resolves #142)

---
+++ package_scripts/l/laipic.perfoo.ps1
@@ -0,0 +1,12 @@
+$result = (Invoke-RestMethod -Uri $package.repo_uri).data.perfooUpdatePC | ConvertFrom-Json
+if ($result.versionCode -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.versionCode
+    $jsonTag = $result.versionCode
+    $urls.Add($result.downloadUrl) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
