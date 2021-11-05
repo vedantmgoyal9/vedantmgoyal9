ccc3295419bec3ba2c78814e46d25d9a694207bb
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-11-05 10:35:33 +0000

    Add UserAgent (nutstore.nutstore) [#133]

--- package_scripts/n/nutstore.nutstore.ps1
+++ package_scripts/n/nutstore.nutstore.ps1
@@ -1,4 +1,4 @@
-$result = ((Invoke-RestMethod $package.repo_uri) | Where-Object { $_.OS -eq 'win-wpf-client' }).stVer
+$result = ((Invoke-RestMethod $package.repo_uri -UserAgent 'NutstoreApp-6.2.4') | Where-Object { $_.OS -eq 'win-wpf-client' }).stVer
 if ($result -gt $package.last_checked_tag) {
     $update_found = $true
     $version = $result
