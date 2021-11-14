dc91072b0b518ee6884bce50731b519471d46b7c
winget-pkgs-automation <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-11-14 07:45:34 +0000

    Fix discord scripts

--- package_scripts/d/discord.discord.ps1
+++ package_scripts/d/discord.discord.ps1
@@ -1,11 +1,11 @@
-$result = Invoke-WebRequest -Uri $package.repo_uri -Method Head
-$versionFromInstallerUrl = ($result.BaseResponse.ResponseUri.Segments -match '[0-9]\.').TrimEnd('/')
+$result = (Invoke-WebRequest -Uri $package.repo_uri -Method Head).BaseResponse.RequestMessage.RequestUri.OriginalString
+$versionFromInstallerUrl = ($result | Select-String -Pattern '[0-9.]{3,}').Matches.Value
 if ($versionFromInstallerUrl -gt $package.last_checked_tag)
 {
     $update_found = $true
     $version = $versionFromInstallerUrl
     $jsonTag = $versionFromInstallerUrl
-    $urls.Add($result.BaseResponse.ResponseUri.AbsoluteUri) | Out-Null
+    $urls.Add($result) | Out-Null
 }
 else
 {
