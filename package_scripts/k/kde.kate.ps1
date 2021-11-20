d8a6fb27582ebdd58045041b16614b43626f5cd7
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-11-20 18:18:58 +0000

    KDE.Cantor

--- package_scripts/k/kde.kate.ps1
+++ package_scripts/k/kde.kate.ps1
@@ -2,7 +2,7 @@ $result = (Invoke-RestMethod -Uri $package.repo_uri).lastSuccessfulBuild
 if ($result.number -gt $package.last_checked_tag)
 {
     $update_found = $true
-    $installerUrl = "$($result.url)artifact/$((Invoke-RestMethod -Uri "$($result.url)api/json").artifacts.FileName -match '.*.exe$')"
+    $installerUrl = "$($result.url)artifact/$((Invoke-RestMethod -Uri "$($result.url)api/json").artifacts.FileName -match $package.asset_regex)"
     $version = ($installerUrl | Select-String -Pattern "[0-9.]{5,}").Matches.Value
     $jsonTag = $result.number.ToString()
     $urls.Add($installerUrl) | Out-Null
