b3d33ed23fe0b0a689f9f09a2a6ebed39dc95091
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2021-11-22 11:38:33 +0100

    add jellyfin.jellyfinserver (#194)
    * add jellyfin.jellyfinserver
    
    * fix $version
    
    * Update jellyfin.jellyfinserver.ps1
    
    * Update jellyfin.jellyfinserver.json
    
    Co-authored-by: Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>

---
+++ package_scripts/j/jellyfin.jellyfinserver.ps1
@@ -0,0 +1,13 @@
+$result = Invoke-WebRequest $package.repo_uri -UseBasicParsing
+$installerUrl = "$package.repo_uri/$(($result.Links | Where-Object { $_.href -match "installer\/jellyfin_(.*)_windows-x64\.exe$" }).href)"
+$version = $Matches[1]
+if ($version -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $jsonTag = $version
+    $urls.Add($installerUrl) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
