6c995e48c8f98375218d4489928d1c9fa57fdb02
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2021-11-22 08:44:09 +0100

    add jellyfin.jellyfinserver

---
+++ package_scripts/j/jellyfin.jellyfinserver.ps1
@@ -0,0 +1,15 @@
+$r = Invoke-WebRequest $package.repo_uri -UseBasicParsing
+$installerUrl = "$package.repo_uri$($r.Links | Where-Object { $_.href -match "installer\/jellyfin_(.*)_windows-x64\.exe$" } | Select-Object -ExpandProperty HREF)"
+$version = $Matches[1]
+
+if ($version -gt $package.last_checked_tag)
+{
+    $update_found = $true
+    $version = $result.version
+    $jsonTag = $result.version
+    $urls.Add($installerUrl) | Out-Null
+}
+else
+{
+    $update_found = $false
+}
