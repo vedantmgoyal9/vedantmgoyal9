6b232b7349e209e875a361f88fa4732db20d9fd8
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-11-22 16:06:32 +0530

    Update jellyfin.jellyfinserver.ps1

--- package_scripts/j/jellyfin.jellyfinserver.ps1
+++ package_scripts/j/jellyfin.jellyfinserver.ps1
@@ -1,7 +1,6 @@
-$r = Invoke-WebRequest $package.repo_uri -UseBasicParsing
-$installerUrl = "$package.repo_uri$($r.Links | Where-Object { $_.href -match "installer\/jellyfin_(.*)_windows-x64\.exe$" } | Select-Object -ExpandProperty HREF)"
+$result = Invoke-WebRequest $package.repo_uri -UseBasicParsing
+$installerUrl = "$package.repo_uri/$(($result.Links | Where-Object { $_.href -match "installer\/jellyfin_(.*)_windows-x64\.exe$" }).href)"
 $version = $Matches[1]
-
 if ($version -gt $package.last_checked_tag)
 {
     $update_found = $true
