68525fb6957ecf57c9ab879168fee73ebb394339
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-11-23 10:43:11 +0000

    Update Jellyfin.JellyfinServer

--- package_scripts/j/jellyfin.jellyfinserver.ps1
+++ package_scripts/j/jellyfin.jellyfinserver.ps1
@@ -1,11 +1,10 @@
-$result = Invoke-WebRequest $package.repo_uri -UseBasicParsing
-$installerUrl = "$($package.repo_uri)/$(($result.Links | Where-Object { $_.href -match "installer\/jellyfin_(.*)_windows-x64\.exe$" }).href)"
-$version = $Matches[1]
-if ($version -gt $package.last_checked_tag)
+$result = Invoke-RestMethod -Method Get -Uri "https://api.github.com/repos/$($package.repo_uri)/releases?per_page=1" -Headers $ms_header
+if ($result.prerelease -eq $package.is_prerelease -and $result.id -gt $package.last_checked_tag)
 {
     $update_found = $true
-    $jsonTag = $version
-    $urls.Add($installerUrl) | Out-Null
+    $version = $result.tag_name.TrimStart('v')
+    $jsonTag = $result.id.ToString()
+    $urls.Add("https://repo.jellyfin.org/releases/server/windows/stable/installer/jellyfin_$($version)_windows-x64.exe") | Out-Null
 }
 else
 {
