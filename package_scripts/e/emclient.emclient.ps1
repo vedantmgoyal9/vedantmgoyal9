2b17e7d7b8fba2e985d91d12deee2902a71280ce
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-12-17 09:10:05 +0000

    fix(custom script): replace url with package.repo_uri

--- package_scripts/e/emclient.emclient.ps1
+++ package_scripts/e/emclient.emclient.ps1
@@ -1,4 +1,4 @@
-$result = ((Invoke-WebRequest -Uri https://www.emclient.com/dist/latest/setup.msi -Method Head).BaseResponse.RequestMessage.RequestUri.OriginalString | Select-String -Pattern ".*(?=\?)").Matches.Value
+$result = ((Invoke-WebRequest -Uri $package.repo_uri -Method Head).BaseResponse.RequestMessage.RequestUri.OriginalString | Select-String -Pattern ".*(?=\?)").Matches.Value
 $versionFromInstallerUrl = [version](($result | Select-String -Pattern '(?<=v)[0-9.]+').Matches.Value)
 if ($versionFromInstallerUrl -gt [version]$package.last_checked_tag)
 {
