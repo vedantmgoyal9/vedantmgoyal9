452c2f324c39e9dc3b80aa1695e767fe7e2d0eb7
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-01-19 04:15:36 +0000

    fix: fenghe.focusnote and fenghe.migi conflict

--- package_scripts/f/fenghe.migi.ps1
+++ package_scripts/f/fenghe.migi.ps1
@@ -1,7 +1,7 @@
-$body = @"
-platform = '0'
-prodNo = '1'
-"@
+$body = @{
+    platform = '0'
+    prodNo   = '1'
+}
 $result = (Invoke-RestMethod -Method Post -Uri $package.repo_uri -Body $body).data[0].downloadUrl
 $versionFromInstallerUrl = ([Uri]::UnescapeDataString($result) | Select-String -Pattern "[0-9.-]{2,}").Matches.Value.TrimEnd('.')
 if ($versionFromInstallerUrl -gt $package.last_checked_tag)
