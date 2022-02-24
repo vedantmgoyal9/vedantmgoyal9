3732eb40105fbe85e7ac1e822d43c66f5311a114
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-02-24 23:42:35 +0530

    chore: merge all repos

---
+++ java_programs/patterns/pdf/pdf_10.java
@@ -0,0 +1,20 @@
+package java_programs.patterns.pdf;
+/*  d c b a
+    c b a
+    b a
+    a
+*/
+import java.util.Scanner;
+public class pdf_10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no. of lines to print : ");
+        int lines=sc.nextInt();
+        for(int i=lines;i>=1;i--) {
+            for(int j=i;j>=1;j--)
+                System.out.print((char)(96+j)+" ");
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ java_programs/patterns/pdf/pdf_11.java
@@ -0,0 +1,20 @@
+package java_programs.patterns.pdf;
+/*  1 2 3 4
+    2 4 6
+    3 6
+    4
+*/
+import java.util.Scanner;
+public class pdf_11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no. of lines to print : ");
+        int lines=sc.nextInt(),c=1;
+        for(int i=lines;i>=1;i--,c++) {
+            for(int j=1;j<=i;j++)
+                System.out.print(j*c+" ");
+            System.out.println();
+        }
+        sc.close();
+    }
+}
