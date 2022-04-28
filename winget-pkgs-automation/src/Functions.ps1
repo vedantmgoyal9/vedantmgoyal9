9c3c7d5a3770a8660d4479985ec918cf07d8cdcc
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-04-29 00:46:39 +0530

    feat(wpa): arp metadata validation

--- winget-pkgs-automation/src/Functions.ps1
+++ winget-pkgs-automation/src/Functions.ps1
@@ -1,3 +1,50 @@
+# Make function available to local scope so that it is automatically imported to script scope
+Function Local:Test-ArpMetadata {
+    Param (
+        [Parameter(Mandatory = $true)]
+        [System.String] $ManifestFolder
+    )
+    $FunctionsForJob = {
+        # Function to get the add/remove programs entries from the registries
+        Function Get-ARPTable {
+            $RegistryPaths = @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
+            return Get-ItemProperty -Path $RegistryPaths -ErrorAction SilentlyContinue |
+            Select-Object DisplayName, DisplayVersion, Publisher, @{ N = 'ProductCode'; E = { $_.PSChildName } } |
+            Where-Object { $Null -ne $_.DisplayName -and $_.SystemComponent -ne 1 }
+        }
+    }
+    # Get the data needed to compare the ARP metadata from the manifest
+    $ManifestPath = (Resolve-Path -Path $ManifestFolder).Path
+    $ManifestRaw = (& $WinGetDev show --manifest $ManifestPath) # Inside the brackets is done intentionally
+    $Manifest = @($ManifestRaw[0] -replace 'Found', 'PackageName:' -replace '\[.*\..*\]', '') + $ManifestRaw.Where({ $_ -match ':' }) | ConvertFrom-StringData -Delimiter ':'
+    $ProductCode = (Get-Content -Path (Get-ChildItem -Path $ManifestPath -Filter '*installer*').FullName | ConvertFrom-Yaml).Installers.Where({ $_.InstallerUrl -eq $Manifest.'Download Url' }).ProductCode
+    # Install the manifest, compare the changes in the arp table, and output the result to the JSON file
+    $Job = Start-Job -InitializationScript $FunctionsForJob -Name 'InstallAndGetArp' -ScriptBlock {
+        $ArpBeforeInstall = Get-ARPTable # Get Add/Remove Programs entries before installing the package
+        & $Using:WinGetDev install --manifest "$Using:ManifestPath" --no-vt # Install the manifest
+        # Get the difference between current and before-install Add/Remove Programs entries and output them in JSON format to a file
+        ConvertTo-Json -InputObject @(Compare-Object -ReferenceObject (Get-ARPTable) -DifferenceObject $ArpBeforeInstall -Property DisplayName, DisplayVersion, Publisher, ProductCode | Select-Object -Property * -ExcludeProperty SideIndicator) | Set-Content -Path .\Arp-Difference.json
+    } | Wait-Job -Timeout 120 # Timeout after 2 minutes
+    $Job | Receive-Job -ErrorAction SilentlyContinue # Get the stdout of the job
+    $ArpDiff = Get-Content -Path .\Arp-Difference.json -Raw | ConvertFrom-Json # Read the difference JSON file
+    # Prepare pull request body with the differences
+    $PrBody = @"
+### Result: Installation $(($Job.State -eq 'Completed') ? 'Successful' : 'Failed')
+|             | Manifest                 | Add/Remove Programs                   |
+| ----------- | ------------------------ | ------------------------------------- |
+| Name        | $($Manifest.PackageName) | $($ArpDiff.DisplayName -join ', ')    |
+| Version     | $($Manifest.Version)     | $($ArpDiff.DisplayVersion -join ', ') |
+| Publisher   | $($Manifest.Publisher)   | $($ArpDiff.Publisher -join ', ')      |
+| ProductCode | $($ProductCode)          | $($ArpDiff.ProductCode -join ', ')    |
+#### Auto-updated by [vedantmgoyal2009/winget-pkgs-automation](https://github.com/vedantmgoyal2009/vedantmgoyal2009/tree/main/winget-pkgs-automation) in workflow run [$($env:GITHUB_RUN_NUMBER)](<https://github.com/vedantmgoyal2009/vedantmgoyal2009/actions/runs/$($env:GITHUB_RUN_ID)>)
+"@
+    # Display the add/remove programs entries of the package, for logging purposes in github actions
+    Write-Output -InputObject $ArpDiff | Format-List -Property *
+    # Clear variable to prevent old data being inserted in the pull request body of the next package
+    Remove-Variable -Name ArpDiff -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path .\Arp-Difference.json -Force -ErrorAction SilentlyContinue # Remove the difference JSON file after it's been read
+}
+
 Function Read-VersionFromInstaller {
     [OutputType([System.String])]
     Param (
