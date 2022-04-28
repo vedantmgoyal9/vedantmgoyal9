d388a60a64c7acc52ce6512ac3c3cc4a346b0c01
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-04-29 00:46:39 +0530

    feat(wpa): arp metadata validation

--- winget-pkgs-automation/src/Functions.ps1
+++ winget-pkgs-automation/src/Functions.ps1
@@ -1,3 +1,41 @@
+Function Test-ArpMetadata {
+    Param (
+        [Parameter(Mandatory = $true)]
+        [System.String] $ManifestFolder
+    )
+    # Function to get the add/remove programs entries from the registries
+    Function Get-ARPTable {
+        $RegistryPaths = @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
+        return Get-ItemProperty -Path $RegistryPaths -ErrorAction SilentlyContinue | 
+        Select-Object DisplayName, DisplayVersion, Publisher, @{ N = 'ProductCode'; E = { $_.PSChildName } } |
+        Where-Object { $Null -ne $_.DisplayName -and $_.SystemComponent -ne 1 }
+    }
+    # Get the data needed to compare the ARP metadata from the manifest
+    $ManifestPath = (Resolve-Path -Path $ManifestFolder).Path
+    $ManifestRaw = (& $WinGetDev show --manifest $ManifestPath) # Inside the brackets is done intentionally
+    $Manifest = @($ManifestRaw[0] -replace 'Found', 'PackageName:' -replace '\[.*\..*\]', '') + $ManifestRaw.Where({ $_ -match ':' }) | ConvertFrom-StringData -Delimiter ':'
+    $ProductCode = (Get-Content -Path (Get-ChildItem -Path $ManifestPath -Filter '*installer*').FullName | ConvertFrom-Yaml).Installers.Where({ $_.InstallerUrl -eq $Manifest.'DownloadUrl' }).ProductCode
+    # Get Add/Remove Programs entries before installing the package
+    $ArpBeforeInstall = Get-ARPTable
+    # Install the manifest
+    & $WinGetDev install --manifest $ManifestPath --no-vt
+    # Get the difference between current and before-install Add/Remove Programs entries
+    $ArpDiff = Compare-Object -ReferenceObject (Get-ARPTable) -DifferenceObject $ArpBeforeInstall -Property DisplayName, DisplayVersion, Publisher, ProductCode | Select-Object -Property * -ExcludeProperty SideIndicator
+    # Prepare pull request body with the differences
+    $PrBody = @"
+|             | Manifest                 | ARP                        |
+| ----------- | ------------------------ | -------------------------- |
+| Name        | $($Manifest.PackageName) | $($ArpDiff.DisplayName)    |
+| Version     | $($Manifest.Version)     | $($ArpDiff.DisplayVersion) |
+| Publisher   | $($Manifest.Publisher)   | $($ArpDiff.Publisher)      |
+| ProductCode | $($ProductCode)          | $($ArpDiff.ProductCode)    |
+    
+#### Auto-updated by [vedantmgoyal2009/winget-pkgs-automation](https://github.com/vedantmgoyal2009/vedantmgoyal2009/tree/main/winget-pkgs-automation) in workflow run [$($env:GITHUB_RUN_NUMBER)](https://github.com/vedantmgoyal2009/vedantmgoyal2009/actions/runs/$($env:GITHUB_RUN_ID))
+"@
+    # Display the add/remove programs entries of the package, for logging purposes in github actions
+    Write-Output -InputObject $ArpDiff | Format-List -Property *
+}
+
 Function Read-VersionFromInstaller {
     [OutputType([System.String])]
     Param (
