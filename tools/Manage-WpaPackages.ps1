efa4d55d425683d09b9ee2c279b6d9ce14fa451d
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-08-12 18:45:51 +0530

    feat: github-bot, whatsapp-bot fixes, other minor changes
    - Update package.json, package-lock.json (npm lockfile)
    - Fix issues in whatsapp-bot.js and remove some env variables
    - [Docs] wpa-packages.md is now maintained by github-bot.js
    - [CI workflow] Remove winget-pkgs-automation (job) pester tests and code coverage
    - Removed vscode's saved workspace (.devcontainer)
    - Update pull_request_template, .prettierignore
    - winget-pkgs-automation (functions.ps1):
      - Do NOT force-push on OpenPRs, instead revert the previous commit, and
        cherry-pick the new commit
      - Remove unused if conditions
    - [WPA workflow]: use IIFE (Immediately Invoked Function Expression) in auth.js
    - [Tools] Merge WPA scripts, move files from subfolder to root, and
      update Test-NsisOrInno.ps1 accordingly

--- tools/Manage-WpaPackages.ps1
+++ tools/Manage-WpaPackages.ps1
@@ -7,28 +7,45 @@
 
 Param (
     [Parameter(
-        Mandatory = $false,
+        Mandatory = $true,
         Position = 0,
         HelpMessage = 'The PackageIdentifier of the package to get the updates for.'
     )]
     [ValidateNotNullOrEmpty()]
-    [System.String] $PackageIdentifier
+    [System.String] $PackageIdentifier,
+
+    [Parameter(
+        Mandatory = $false,
+        Position = 1,
+        HelpMessage = 'Only perform validation of the package json file.'
+    )]
+    [System.Management.Automation.SwitchParameter] $TestPackage
 )
 
 <#
 .SYNOPSIS
-    winget-pkgs-automation package json creator
+    winget-pkgs-automation package json creator and tester
 .DESCRIPTION
-    this script gets various parameters about the package from the user
-    and creates a json file for the package to be used by the winget-pkgs-automation
+    this script gets various parameters about the package from the user, and
+    creates as well as validates the package json file so that it can be used by the
+    winget-pkgs-automation project to automatically update them at winget-pkgs repo.
 .NOTES
     please file an issue if you run into errors with the script:
     https://github.com/vedantmgoyal2009/vedantmgoyal2009/issues/
 .LINK
-    https://github.com/vedantmgoyal2009/vedantmgoyal2009/blob/main/tools/New-Package.ps1
+    https://github.com/vedantmgoyal2009/vedantmgoyal2009/blob/main/tools/Manage-WpaPackages.ps1
 #>
 
 Begin {
+    # HIDE PROGRESS BAR OF INVOKE-WEBREQUEST #
+    #
+    # To hide the progress bar of Invoke-WebRequest, we need to set the
+    # $ProgressPreference variable to 'SilentlyContinue'
+    #
+    ###########################################
+
+    $ProgressPreference = 'SilentlyContinue'
+
     ########## FUNCTION DEFINITIONS ###########
     #
     # This section contains definitions of all the functions used elsewhere in this script.
@@ -196,47 +213,198 @@ Begin {
             }
         }
     }
+
+    Function Read-VersionFromInstaller {
+        [OutputType([System.String])]
+        Param (
+            [Parameter(Mandatory = $true)]
+            [System.String] $Uri,
+
+            [Parameter(Mandatory = $true)]
+            [System.String] $Property
+        )
+        $FileName = Join-Path -Path $env:TEMP -ChildPath ([System.IO.Path]::GetFileName(([System.Uri] $Uri).LocalPath))
+        Invoke-WebRequest -Uri $Uri -OutFile $FileName
+        If ([System.IO.Path]::GetExtension($FileName) -eq '.msi') {
+            $WindowsInstaller = New-Object -Com WindowsInstaller.Installer
+            $MSI = $WindowsInstaller.OpenDatabase($FileName, 0)
+            $_TablesView = $MSI.OpenView('SELECT * FROM _Tables')
+            $_TablesView.Execute()
+            $_Database = @{}
+            do {
+                $_Table = $_TablesView.Fetch()
+                If ($_Table) {
+                    $_TableName = $_Table.GetType().InvokeMember('StringData', 'Public, Instance, GetProperty', $Null, $_Table, 1)
+                    $_Database["$_TableName"] = @{}
+                }
+            } while ($_Table)
+            [void][System.Runtime.InteropServices.Marshal]::FinalReleaseComObject($_TablesView)
+            ForEach ($_Table in $_Database.Keys) {
+                $_ItemView = $MSI.OpenView("SELECT * FROM $_Table")
+                $_ItemView.Execute()
+                do {
+                    $_Item = $_ItemView.Fetch()
+                    If ($_Item) {
+                        $_ItemValue = $Null
+                        $_ItemName = $_Item.GetType().InvokeMember('StringData', 'Public, Instance, GetProperty', $Null, $_Item, 1)
+                        If ($_Table -eq 'Property') {
+                            try {
+                                $_ItemValue = $_Item.GetType().InvokeMember('StringData', 'Public, Instance, GetProperty', $Null, $_Item, 2)
+                            } catch {
+                                Out-Null
+                            }
+                        }
+                        $_Database.$_Table["$_ItemName"] = $_ItemValue
+                    }
+                } while ($_Item)
+                [void][System.Runtime.InteropServices.Marshal]::FinalReleaseComObject($_ItemView)
+            }
+            [void][System.Runtime.InteropServices.Marshal]::FinalReleaseComObject($MSI)
+            [void][System.Runtime.InteropServices.Marshal]::FinalReleaseComObject($WindowsInstaller)
+            $PkgVersion = $_Database.Property."$Property"
+        } Else {
+            $MetaDataObject = [ordered] @{}
+            $FileInformation = Get-Item $FileName
+            $ShellFolder = (New-Object -ComObject Shell.Application).Namespace($FileInformation.Directory.FullName)
+            $ShellFile = $ShellFolder.ParseName($FileInformation.Name)
+            $MetaDataProperties = [ordered] @{}
+            0..400 | ForEach-Object -Process {
+                $DataValue = $ShellFolder.GetDetailsOf($Null, $_)
+                $PropertyValue = (Get-Culture).TextInfo.ToTitleCase($DataValue.Trim()).Replace(' ', '')
+                If ($PropertyValue -ne '') {
+                    $MetaDataProperties["$_"] = $PropertyValue
+                }
+            }
+            ForEach ($Key in $MetaDataProperties.Keys) {
+                $MetaDataProperty = $MetaDataProperties[$Key]
+                $Value = $ShellFolder.GetDetailsOf($ShellFile, [int] $Key)
+                If ($MetaDataProperty -in 'Attributes', 'Folder', 'Type', 'SpaceFree', 'TotalSize', 'SpaceUsed') {
+                    continue
+                }
+                If (($Null -ne $Value) -and ($Value -ne '')) {
+                    $MetaDataObject["$MetaDataProperty"] = $Value
+                }
+            }
+            [void][System.Runtime.InteropServices.Marshal]::FinalReleaseComObject($ShellFile)
+            [void][System.Runtime.InteropServices.Marshal]::FinalReleaseComObject($ShellFolder)
+            $PkgVersion = $MetaDataObject."$Property"
+        }
+        [System.GC]::Collect()
+        [System.GC]::WaitForPendingFinalizers()
+        # Remove-Item -Path $FileName -Force
+        return $PkgVersion
+    }
+
+    Function Test-Package {
+        Param (
+            [Parameter(
+                Mandatory = $true,
+                Position = 0,
+                HelpMessage = 'PSObject of the package json file'
+            )]
+            [System.Management.Automation.PSObject] $PackageObject
+        )
+
+        $_Object = New-Object -TypeName System.Management.Automation.PSObject
+        $_Object | Add-Member -MemberType NoteProperty -Name 'PackageIdentifier' -Value $PackageObject.Identifier
+        $VersionRegex = $PackageObject.VersionRegex
+        $InstallerRegex = $PackageObject.InstallerRegex
+        If (-not [System.String]::IsNullOrEmpty($PackageObject.AdditionalInfo)) {
+            $PackageObject.AdditionalInfo.PSObject.Properties | ForEach-Object {
+                Set-Variable -Name $_.Name -Value $_.Value
+            }
+        }
+        $Parameters = @{ Method = $PackageObject.Update.Method; Uri = $PackageObject.Update.Uri }
+        If (-not [System.String]::IsNullOrEmpty($PackageObject.Update.Headers)) {
+            $PackageObject.Update.Headers.PSObject.Properties | ForEach-Object -Begin { $Headers = @{} } -Process { If ($_.Value -notcontains "`$AuthToken") { $Headers.Add($_.Name, $_.Value) } } -End { $Parameters.Headers = $Headers }
+        }
+        If (-not [System.String]::IsNullOrEmpty($PackageObject.Update.Body)) {
+            $Parameters.Body = $PackageObject.Update.Body
+        }
+        If (-not [System.String]::IsNullOrEmpty($PackageObject.Update.UserAgent)) {
+            $Parameters.UserAgent = $PackageObject.Update.UserAgent
+        }
+        If ($PackageObject.Update.InvokeType -eq 'RestMethod') {
+            $Response = Invoke-RestMethod @Parameters
+        } ElseIf ($PackageObject.Update.InvokeType -eq 'WebRequest') {
+            $Response = Invoke-WebRequest @Parameters
+        }
+        If (-not [System.String]::IsNullOrEmpty($PackageObject.PostResponseScript)) {
+            $PackageObject.PostResponseScript | Invoke-Expression # Run PostResponseScript
+        }
+        $PackageObject.ManifestFields.PSObject.Properties | ForEach-Object {
+            # If Read-VersionFromInstaller function is being called, inform the
+            # user that it may take some time to download the installer
+            If ($_.Value -match 'Read-VersionFromInstaller') {
+                Write-Output 'Downloading the installer to get the version... This may take some time...'
+            }
+            $_Object | Add-Member -MemberType NoteProperty -Name $_.Name -Value ($_.Value | Invoke-Expression)
+        }
+
+        Write-Output -InputObject $_Object | Format-List -Property *
+
+        # Perform validation of the response object
+        $_IsValid = $true
+        $_Object.PSObject.Properties | ForEach-Object {
+            If ($Null -eq $_.Value) {
+                Write-Error "$($_.Name) doesn't have a value, it's empty"
+                $_IsValid = $false
+            }
+        }
+        If ($_IsValid -eq $true) {
+            Write-Output 'The package is valid!'
+        } Else {
+            Write-Error "Some values are missing or empty, please fix the json file manually and run:`n   .\Manage-WpaPackages.ps1 $($Package.Identifier) -TestPackage"
+        }
+    }
 }
 
 Process {
-    $Package = [ordered] @{
-        '$schema'          = 'https://github.com/vedantmgoyal2009/vedantmgoyal2009/raw/main/src/winget-pkgs-automation/schema.json';
-        Identifier         = '';
-        Update             = [ordered] @{
-            InvokeType = '';
-            Uri        = '';
-            Method     = '';
-            Headers    = @{};
-            Body       = '';
-            UserAgent  = ''
-        };
-        PostResponseScript = '';
-        VersionRegex       = '';
-        InstallerRegex     = '';
-        PreviousVersion    = '';
-        ManifestFields     = [ordered] @{
-            PackageVersion = '';
-            InstallerUrls  = '';
-        };
-        AdditionalInfo     = @{};
-        PostUpgradeScript  = '';
-        YamlCreateParams   = [ordered] @{
-            SkipPRCheck           = $false;
-            AutoUpgrade           = $false;
-            DeletePreviousVersion = $false;
-        };
-        SkipPackage        = $false
-    }
+    $PackageJsonPath = "$PSScriptRoot\..\src\winget-pkgs-automation\packages\$($PackageIdentifier.Substring(0,1).ToLower())\$($PackageIdentifier.ToLower()).json"
 
-    If ($PSBoundParameters.ContainsKey('PackageIdentifier')) {
-        $Package.Identifier = $PackageIdentifier
+    If ($TestPackage) {
+        $Package = Get-Content -Raw $PackageJsonPath | ConvertFrom-Json
+        Test-Package -PackageObject $Package
+        return
     } Else {
-        Write-Output 'Enter PackageIdentifier of the package (Example: JanDeDobbeleer.OhMyPosh)'
-        $Package.Identifier = Get-UserInput -Method String -Message 'PackageIdentifier'
-        Write-Output ''
+        $Package = [System.Management.Automation.PSObject] [ordered] @{
+            '$schema'          = 'https://github.com/vedantmgoyal2009/vedantmgoyal2009/raw/main/src/winget-pkgs-automation/schema.json';
+            Identifier         = $PackageIdentifier;
+            Update             = [ordered] @{
+                InvokeType = '';
+                Uri        = '';
+                Method     = '';
+                Headers    = @{};
+                Body       = '';
+                UserAgent  = ''
+            };
+            PostResponseScript = '';
+            VersionRegex       = '';
+            InstallerRegex     = '';
+            PreviousVersion    = '';
+            ManifestFields     = [ordered] @{
+                PackageVersion = '';
+                InstallerUrls  = '';
+            };
+            AdditionalInfo     = @{};
+            PostUpgradeScript  = '';
+            YamlCreateParams   = [ordered] @{
+                SkipPRCheck           = $false;
+                AutoUpgrade           = $false;
+                DeletePreviousVersion = $false;
+            };
+            SkipPackage        = $false
+        }
     }
 
-    $PackageJsonPath = "$PSScriptRoot\..\src\winget-pkgs-automation\packages\$($Package.Identifier.Substring(0,1).ToLower())\$($Package.Identifier.ToLower()).json"
+    # Check if the package json file already exists
+    If (Test-Path -Path $PackageJsonPath -PathType Leaf) {
+        Write-Output 'The package already exists, do you still want to continue?'
+        # Return values are inverted because we want to 'return' if user doesn't want to continue
+        If (Get-UserInput -Method KeyPress -Message 'Choice (y/n): ' -ReturnValues @{ Y = $false; N = $true }) {
+            return
+        }
+    }
 
     Write-Output 'Is the package a GitHub package? (meaning: the package is hosted on GitHub)'
     Write-Output 'This will set the following properties automatically:'
@@ -453,25 +621,12 @@ Process {
     Write-Output 'Do you want to test the package?'
     If (Get-UserInput -Method KeyPress -Message 'Choice (y/n): ' -ReturnValues @{ Y = $true; N = $false }) {
         Write-Output ''
-        $PackageObject = & $PSScriptRoot\Test-Package.ps1 -PackageIdentifier $Package.Identifier
-        Write-Output -InputObject $PackageObject | Format-List -Property *
-        $PackageIsValid = $true
-        $PackageObject.PSObject.Properties | ForEach-Object {
-            If ($Null -eq $_.Value) {
-                Write-Output "$($_.Name) doesn't have a value, it's empty"
-                $PackageIsValid = $false
-            }
-        }
-        If ($PackageIsValid -eq $true) {
-            Write-Output 'The package is valid!'
-        } Else {
-            Write-Output "Some values are missing or empty, please fix the JSON file manually and run:`n   .\Test-Package.ps1 -PackageIdentifier $($Package.Identifier)"
-        }
+        Test-Package -PackageObject $Package
     }
 }
 
 End {
-    Write-Output 'Do you want to create another package?'
+    Write-Output 'Do you want to create/test another package?'
     If (Get-UserInput -Method KeyPress -Message 'Choice (y/n): ' -ReturnValues @{ Y = $true; N = $false }) {
         & $PSCommandPath
     }
--- tools/Test-NsisOrInno.ps1
+++ tools/Test-NsisOrInno.ps1
@@ -5,22 +5,22 @@ Param (
 )
 
 # This code block looks dirty because Test-Path does not work as expected 😠
-If ([System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\innounp.exe") -and
-    [System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\PEiD.exe") -and
-    [System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\AutoItX\AutoItX.psd1") -and
-    [System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\AutoItX\AutoItX3_x64.dll") -and
-    [System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\AutoItX\AutoItX3.Assembly.dll") -and
-    [System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\AutoItX\AutoItX3.dll") -and
-    [System.IO.File]::Exists("$PSScriptRoot\Test-NsisOrInno\AutoItX\AutoItX3.PowerShell.dll")
+If ([System.IO.File]::Exists("$PSScriptRoot\innounp.exe") -and
+    [System.IO.File]::Exists("$PSScriptRoot\PEiD.exe") -and
+    [System.IO.File]::Exists("$PSScriptRoot\AutoItX\AutoItX.psd1") -and
+    [System.IO.File]::Exists("$PSScriptRoot\AutoItX\AutoItX3_x64.dll") -and
+    [System.IO.File]::Exists("$PSScriptRoot\AutoItX\AutoItX3.Assembly.dll") -and
+    [System.IO.File]::Exists("$PSScriptRoot\AutoItX\AutoItX3.dll") -and
+    [System.IO.File]::Exists("$PSScriptRoot\AutoItX\AutoItX3.PowerShell.dll")
 ) {
-    Import-Module $PSScriptRoot\Test-NsisOrInno\AutoItX\AutoItX.psd1
+    Import-Module $PSScriptRoot\AutoItX\AutoItX.psd1
 } Else {
-    Write-Error 'Please verify that the folder containing the required files is in the same folder as the script.'
+    Write-Error 'Please verify that the folder contains the required files to run the script.'
     Exit 1
 }
 
 Function Get-PEiDString {
-    Invoke-AU3Run -Program "$PSScriptRoot\Test-NsisOrInno\PEiD.exe -hard $InstallerPath" | Out-Null
+    Invoke-AU3Run -Program "$PSScriptRoot\PEiD.exe -hard $InstallerPath" | Out-Null
     Wait-AU3Win -Title 'PEiD v0.9' | Out-Null
     $PEiDWinHandle = Get-AU3WinHandle -Title 'PEiD v0.9'
     Show-AU3WinActivate -WinHandle $PEiDWinHandle | Out-Null
@@ -47,7 +47,7 @@ $FileExt = [System.IO.Path]::GetExtension($InstallerPath)
 If ($FileExt -eq '.exe') {
     $HexBytes = (Format-Hex -Path $InstallerPath -Count 2).HexBytes
     If (($HexBytes -join '' -replace ' ', $Null) -eq '4D5A') {
-        $InnoUnp = & $PSScriptRoot\Test-NsisOrInno\innounp.exe $InstallerPath
+        $InnoUnp = & $PSScriptRoot\innounp.exe $InstallerPath
         If ($InnoUnp[0].Contains('Version detected:')) {
             Write-Output $Msgs.InnoSetup
             Exit 0
