7a30a994db1286633e42f6aecf4e71d0cc3f099f
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-08-12 18:45:51 +0530

    feat: github-bot, whatsapp-bot fixes, other minor changes
    - Update package.json, package-lock.json (npm lockfile)
    - Fix issues in whatsapp-bot.js and remove some env variables
    - [Docs] wpa-packages.md is now maintained by github-bot.js
    - [CI workflow] Remove winget-pkgs-automation (job) pester tests and code coverage
    - Removed vscode's saved workspace (.devcontainer)
    - Update pull_request_template, .prettierignore
    - winget-pkgs-automation (functions.ps1):
      - Do NOT force-push on OpenPRs, instead revert the previous commit, and
        cherry-pick the new commit
      - Remove unused if conditions
    - [WPA workflow]: use IIFE (Immediately Invoked Function Expression) in auth.js
    - [Tools] Merge WPA scripts, move files from subfolder to root, and
      update Test-NsisOrInno.ps1 accordingly

--- src/whatsapp-bot.js
+++ src/whatsapp-bot.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const { readFileSync, createWriteStream } = require('fs');
 const { format } = require('util');
 const { randomBytes } = require('crypto');
-const { generate } = require('qrcode-terminal');
 const { initializeApp } = require('firebase/app');
 const {
   getFirestore,
@@ -12,10 +11,9 @@ const {
 } = require('firebase/firestore/lite');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const server = require('express')();
-const log_file = createWriteStream(__dirname + '/' + process.env.FRBE_PH_FL, {
+const log_file = createWriteStream(`${__dirname}/logs.txt`, {
   flags: 'w',
 });
-const log_stdout = process.stdout;
 const wa = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: { headless: true },
@@ -24,10 +22,10 @@ const wa = new Client({
 // make console.log to output to both console and log file
 console.log = function (log_line) {
   log_file.write(format(log_line) + '\n');
-  log_stdout.write(format(log_line) + '\n');
+  process.stdout.write(format(log_line) + '\n');
 };
 
-// initialize server
+// initialize server, port is PT env variable
 server.listen(process.env.PT, () => {
   console.log('Server started successfully');
 });
@@ -35,7 +33,7 @@ server.listen(process.env.PT, () => {
 // intialize whatsapp
 wa.initialize();
 wa.on('qr', (qr) => {
-  generate(qr, { small: true });
+  process.stdout.write(qr + '\n');
 });
 wa.on('authenticated', () => {
   console.log('Session authentication successful or restored.');
@@ -58,12 +56,12 @@ const database = doc(firestore, process.env.FRBE_CLN, process.env.FRBE_DC);
 
 // authenticate new users and give them a unique key
 wa.on('message', (message) => {
-  if (message.body === process.env.FRBE_RG_MG) {
+  if (message.body === process.env.WA_RG_MG) {
     const auth = randomBytes(16).toString('hex');
     wa.sendMessage(message.from, `Authentication key: ${auth}`);
     updateDoc(database, { [`${message.from.split('@')[0]}`]: auth }).then(
       function () {
-        console.log(`+${message.from.split('@')[0]} has been registered`);
+        console.log(`+${message.from.split('@')[0]} has been registered.`);
       }
     );
   }
@@ -71,40 +69,37 @@ wa.on('message', (message) => {
 
 // make logs available on the internet
 // hide phone number on the internet, privacy is important!
-server.get(process.env.FRBE_LN_PH_LG, function (req, res) {
+server.get('/logs.txt', function (req, res) {
   res.status(200);
   res.send(
-    readFileSync(process.env.FRBE_PH_FL, 'utf8').replace(
-      /(?<=\+[0-9]{3})\d+(?=\s)/g,
-      'XXXXXXXXX'
-    )
+    readFileSync(`${__dirname}/logs.txt`, 'utf8')
+      .replace(/(?<=\+[0-9]{3})\d+(?=\s)/g, 'XXXXXXXXX')
+      .replace(/\n/g, '<br/>') // replace new lines with <br/>
   );
 });
 
 // start listening on server and send the message to the user
 // if the authentication key is valid else send an error message
-server.get(process.env.FRBE_LN_PH, (req, res) => {
-  if (!req.query.sendto || !req.query.text || !req.query.auth) {
+server.get('/', (req, res) => {
+  if (!req.query.to || !req.query.msg || !req.query.auth) {
     res.status(418);
     res.send('Missing parameters.');
     return;
   }
   getDoc(database).then((doc) => {
-    const authInDatabase = doc.data()[`${req.query.sendto}`];
+    const authInDatabase = doc.data()[`${req.query.to}`];
     if (authInDatabase === req.query.auth) {
-      wa.sendMessage(`${req.query.sendto}@c.us`, `${req.query.text}`).then(
-        () => {
-          res.status(200);
-          res.send(`
+      wa.sendMessage(`${req.query.to}@c.us`, `${req.query.msg}`).then(() => {
+        res.status(200);
+        res.send(`
             <b>Status</b>: Successful <br>
-            <b>To</b>: +${req.query.sendto} <br>
-            <b>Message</b>: ${req.query.text}
+            <b>To</b>: +${req.query.to} <br>
+            <b>Message</b>: ${req.query.msg}
           `);
-          console.log(
-            `To: +${req.query.sendto}; Message: ${req.query.text}\n-----------`
-          );
-        }
-      );
+        console.log(
+          `To: +${req.query.to}; Message: ${req.query.msg}\n-----------`
+        );
+      });
     } else if (
       authInDatabase != req.query.auth &&
       authInDatabase != undefined
