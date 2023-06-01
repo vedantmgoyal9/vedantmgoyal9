673c4e3c5ec64a308ae0453b884e79c192787b32
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2023-06-01 13:29:31 +0000

    Approve dependabot PRs, [mrf-bot] remove custom queries

--- mrf-bot/content-scripts/window-open-injection/injector.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener(constants.MESSAGE_TYPES.OPEN_URL_IN_BACKGROUND, e => {
-  chrome.runtime.sendMessage({
-    type: constants.MESSAGE_TYPES.OPEN_URL_IN_BACKGROUND,
-    url: e.detail,
-  });
-});
-
-injectScript('/content-scripts/window-open-injection/main.js');
