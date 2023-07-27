98eed8f54a6ccd9187165f141872fe19e53427fb
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2023-07-27 12:40:03 +0000

    feat(github-bot): remove reviewers and lock pr when dependabot prs are closed

--- api/github-bot.ts
+++ api/github-bot.ts
@@ -10,7 +10,33 @@ function probotApp(app: Probot) {
   app.on(
     'pull_request.closed',
     async (context: Context<'pull_request.closed'>) => {
-      // check if the PR was merged or closed without merging
+      if (
+        'dependabot[bot]' === context.payload.pull_request.user.login &&
+        (await context.octokit.issues.listComments(context.issue())).data.find(
+          (comment) =>
+            [
+              /Superseded by #([0-9]+)/g,
+              /Looks like [@/-a-zA-Z]+ is up-to-date now, so this is no longer needed\./g,
+            ].some((regex) => regex.test(comment.body || '')),
+        )
+      ) {
+        const requestedReviewers =
+          await context.octokit.pulls.listRequestedReviewers(
+            context.pullRequest(),
+          );
+        if (requestedReviewers.data.users.length > 0)
+          await context.octokit.pulls.removeRequestedReviewers(
+            context.pullRequest({
+              reviewers: requestedReviewers.data.users.map(
+                (user) => user.login,
+              ),
+            }),
+          );
+        return await context.octokit.issues.lock(
+          context.issue({ lock_reason: 'resolved' }),
+        );
+      }
+
       if (context.payload.pull_request.merged === true) {
         // lock conversation on the PR
         return await context.octokit.issues.lock(
